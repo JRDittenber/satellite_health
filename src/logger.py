a9dad4c38d2ccf830000b7dbdc6b60c5ff8882e1
JRDittenber <jeff.dittenber@hotmail.com>
2023-04-20 20:06:32 +0700

    still working on data ingestion

--- src/logger.py
+++ src/logger.py
@@ -2,28 +2,16 @@ import logging
 import os
 from datetime import datetime
 
-def setup_logger():
-    LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
-    logs_path = os.path.join(os.getcwd(), "logs")
-    os.makedirs(logs_path, exist_ok=True)
+LOG_FILE=f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+logs_path=os.path.join(os.getcwd(),"logs",LOG_FILE)
+os.makedirs(logs_path,exist_ok=True)
 
-    LOG_FILE_PATH = os.path.join(logs_path, LOG_FILE)
+LOG_FILE_PATH=os.path.join(logs_path,LOG_FILE)
 
-    logger = logging.getLogger()
-    logger.setLevel(logging.INFO)
+logging.basicConfig(
+    filename=LOG_FILE_PATH,
+    format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
+    level=logging.INFO,
 
-    formatter = logging.Formatter("[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s")
 
-    # File handler
-    file_handler = logging.FileHandler(LOG_FILE_PATH)
-    file_handler.setFormatter(formatter)
-    logger.addHandler(file_handler)
-
-    # Stream handler (console)
-    stream_handler = logging.StreamHandler()
-    stream_handler.setFormatter(formatter)
-    logger.addHandler(stream_handler)
-
-    return logger
-
-logger = setup_logger()
+)
